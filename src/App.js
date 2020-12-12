33ea27122ef6b8d0feca0d486846f12dfa2fcfb8
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-12-12 18:49:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,27 @@ import Container from './Components/Container/Container';
 class App extends Component {
   state = {
     contacts: [
-      { id: shortid.generate(), name: 'Rosie Simpson', number: '459-12-56' },
-      { id: shortid.generate(), name: 'Hermione Kline', number: '443-89-12' },
-      { id: shortid.generate(), name: 'Eden Clements', number: '645-17-79' },
-      { id: shortid.generate(), name: 'Annie Copeland', number: '227-91-26' },
+      // { id: shortid.generate(), name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: shortid.generate(), name: 'Hermione Kline', number: '443-89-12' },
+      // { id: shortid.generate(), name: 'Eden Clements', number: '645-17-79' },
+      // { id: shortid.generate(), name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   formSubmitHandler = ({ name, number }) => {
     const contact = {
