f1cd9f64c3553144a1ecf121ab16945f7901dfae
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-12-12 17:58:33 +0200

    Create ContactList.js

---
+++ src/Components/ContactList/ContactList.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import s from './ContactList.module.css';
+
+export default function ContactList({ contacts, onDeleteContact }) {
+  return (
+    <ul className={s.list}>
+      {contacts.map(({ id, name, number }) => (
+        <li className={s.item} key={id}>
+          <p>
+            {name}:{number}
+          </p>
+          <button type="button" onClick={() => onDeleteContact(id)}>
+            Delete contact
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }),
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
+};
